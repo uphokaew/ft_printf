d07d36b0d4394f139309f737da0ab2d290310e07
beok159 <beok159@gmail.com>
2024-01-26 10:38:38 +0700

    fix %s

--- src/ft_printstr.c
+++ src/ft_printstr.c
@@ -6,20 +6,20 @@
 /*   By: uphokaew <uphokaew@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 18:10:25 by uphokaew          #+#    #+#             */
-/*   Updated: 2024/01/26 10:31:33 by uphokaew         ###   ########.fr       */
+/*   Updated: 2024/01/26 10:38:00 by uphokaew         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_printstr(const char *s)
+int	ft_printstr(char *s)
 {
 	int		length;
 
 	length = 0;
-	if (s == NULL)
+	if (!s)
 	{
-		if (write(1,	"(null)", 6) == -1)
+		if (write(1, "(null)", 6) == -1)
 			return (-1);
 		return (6);
 	}
